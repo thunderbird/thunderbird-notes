58397a67076a45cba0474ebf61497caed1e21725
Andrei Hajdukewycz <sancus@off.net>
2017-06-20 18:55:13 -0700

    Add bits and pieces to get the preview template rendering properly.

--- notes.py
+++ notes.py
@@ -1,10 +1,47 @@
 import jinja2
+import markdown
 import os
-from jinja2 import Template
+import yaml
+
+from gettext import NullTranslations
+
+notesfile = "52.2.1.yaml"
+
+with open(notesfile, 'r') as f:
+    doc = yaml.load(f)
+
+def get_bug_search_url():
+    return doc["release"]["bug_search_url"]
+
+def safe_markdown(text):
+    if not text:
+        text = ''
+    return jinja2.Markup(markdown.markdown(text))
+
+def l10n_format_date(text):
+    return text
+
+def url(*args, **kwargs):
+    return ''
+
+class Translation(object):
+    def gettext(self, text):
+        return text
+    ugettext = gettext
+    ngettext = gettext
+
+translator = Translation()
+
+doc["release"]["get_bug_search_url"] = get_bug_search_url
 
 jinja_env = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(os.path.abspath('./template'))
+    extensions = ["jinja2.ext.i18n"],
+    loader = jinja2.FileSystemLoader(os.path.abspath('./template')),
 )
-
-template = latex_jinja_env.get_template('release-notes.html')
-print(template.render(**project))
+jinja_env.globals.update(url=url)
+jinja_env.filters['markdown'] = safe_markdown
+jinja_env.filters['f'] = jinja_env.filters['format']
+jinja_env.filters['l10n_format_date'] = l10n_format_date
+jinja_env.install_gettext_translations(translator)
+template = jinja_env.get_template('release-notes.html')
+print(template.render(**doc))
