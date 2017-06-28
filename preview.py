a32a832eb6a50e3cf17568e0ba67eb932491ff32
Andrei Hajdukewycz <sancus@off.net>
2017-06-27 20:48:14 -0700

    Clean up preview utility and use new release notes class.

--- preview.py
+++ preview.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from loader import ReleaseNotes
 import jinja2
 import markdown
 import os
@@ -9,28 +10,17 @@ import yaml
 
 parser = argparse.ArgumentParser(
     description="""
-    Enter the file name of the .yml file you want to preview.
-    Example: `python preview.py 52.2.1.yml`
+    Enter the version number you want to preview.
+    Example: `python preview.py 52.2.1`
     """, formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("notesfile", help="name of the release notes .yml file to preview")
+parser.add_argument("notesfile", help="version number of the release notes to preview")
 
 if len(sys.argv)==1:
     parser.print_help()
     sys.exit(1)
 
 args = parser.parse_args()
-
-with open(args.notesfile, "r") as f:
-    doc = yaml.load(f)
-
-with open("settings.yml", "r") as f:
-    s = yaml.load(f)
-    doc["feedback"] = s["feedback"]
-    doc["bugzilla"] = s["bugzilla"]
-
-def get_bug_search_url():
-    return doc["release"]["bug_search_url"]
 
 
 def safe_markdown(text):
@@ -51,27 +41,21 @@ def f(s, *args, **kwargs):
     return s.format(*args, **kwargs)
 
 
+# Stub to make gettext work, no actual translation.
 class Translation(object):
     def gettext(self, text):
         return text
     ugettext = gettext
     ngettext = gettext
 
+notes = ReleaseNotes()
+note = notes.notes.get(args.notesfile)
+
+if not note:
+    print "Error: Can't find notes for version '{0}'.".format(args.notesfile)
+    sys.exit(1)
+
 translator = Translation()
-
-# Prepare variables for template rendering.
-doc["release"]["get_bug_search_url"] = get_bug_search_url
-doc["channel_name"] = "Beta"
-doc["channel"] = doc["channel_name"]
-doc["version"] = doc["release"]["version"]
-doc["known_issues"] = []
-doc["new_features"] = []
-for note in doc["notes"]:
-    if note["tag"] == "unresolved":
-        doc["known_issues"].append(note)
-    else:
-        doc["new_features"].append(note)
-
 
 jinja_env = jinja2.Environment(
     extensions = ["jinja2.ext.i18n"],
@@ -85,6 +69,6 @@ jinja_env.install_gettext_translations(translator)
 template = jinja_env.get_template('release-notes.html')
 
 with open("preview.html", "wb") as f:
-    o = template.render(**doc)
+    o = template.render(**note)
     f.write(o.encode('utf8'))
 
