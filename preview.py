34ae6456a591b739dd14305bcfcd20e35e61fde4
Andrei Hajdukewycz <sancus@off.net>
2017-07-31 17:57:19 -0700

    Change preview.py into a looping loader like the readme says it should be.

--- preview.py
+++ preview.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from loader import ReleaseNotes
+import datetime
 import jinja2
 import markdown
 import os
 import sys
+import time
 import yaml
+
+from loader import ReleaseNotes
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 parser = argparse.ArgumentParser(
     description="""
@@ -48,27 +53,60 @@ class Translation(object):
     ugettext = gettext
     ngettext = gettext
 
-notes = ReleaseNotes()
-note = notes.notes.get(args.notesfile)
 
-if not note:
-    print "Error: Can't find notes for version '{0}'.".format(args.notesfile)
-    sys.exit(1)
+class Handler(FileSystemEventHandler):
+    def __init__(self, version):
+        translator = Translation()
 
-translator = Translation()
+        self.jinja_env = jinja2.Environment(
+            extensions = ["jinja2.ext.i18n"],
+            loader = jinja2.FileSystemLoader(os.path.abspath('./template')),
+        )
+        self.jinja_env.globals.update(url=url)
+        self.jinja_env.filters['markdown'] = safe_markdown
+        self.jinja_env.filters['f'] = f
+        self.jinja_env.filters['l10n_format_date'] = l10n_format_date
+        self.jinja_env.install_gettext_translations(translator)
+        self.template = self.jinja_env.get_template('release-notes.html')
+        self.version = version
+        self.updatetime = datetime.datetime.fromtimestamp(0)
+        self.updatepreview()
 
-jinja_env = jinja2.Environment(
-    extensions = ["jinja2.ext.i18n"],
-    loader = jinja2.FileSystemLoader(os.path.abspath('./template')),
-)
-jinja_env.globals.update(url=url)
-jinja_env.filters['markdown'] = safe_markdown
-jinja_env.filters['f'] = f
-jinja_env.filters['l10n_format_date'] = l10n_format_date
-jinja_env.install_gettext_translations(translator)
-template = jinja_env.get_template('release-notes.html')
 
-with open("preview.html", "wb") as f:
-    o = template.render(**note)
-    f.write(o.encode('utf8'))
+    def updatepreview(self):
+        notes = ReleaseNotes()
+        note = notes.notes.get(self.version)
+        if not note:
+            print "Error: Can't find notes for version '{0}'.".format(self.version)
+            sys.exit(1)
+        with open("preview.html", "wb") as f:
+            o = self.template.render(**note)
+            f.write(o.encode('utf8'))
 
+
+    def throttle_updates(self, timestamp):
+        # Multiple FileModified events can fire, so only update
+        # once per second.
+        delta = timestamp - self.updatetime
+        if delta.seconds > 0:
+            self.updatepreview()
+            print "{0}: Updated preview.html\n".format(timestamp.strftime("%H:%M:%S"))
+            self.updatetime = datetime.datetime.now()
+
+
+    def on_modified(self, event):
+        self.throttle_updates(datetime.datetime.now())
+
+
+handler = Handler(args.notesfile)
+observer = Observer()
+observer.schedule(handler, path='beta', recursive=False)
+observer.schedule(handler, path='release', recursive=False)
+observer.start()
+print "Updating preview.html every time data is modified, press Ctrl-C to end."
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
