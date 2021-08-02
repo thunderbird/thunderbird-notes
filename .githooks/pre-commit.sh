3bf6cc90f83c8ac5802ee4a7def7f0c8eb7ef12e
Dave Miller <justdave@thunderbird.net>
2021-08-02 13:45:24 -0400

    pre-commit hook to yamllint before commit (#17)
    * pre-commit hook to yamllint before commit

---
+++ .githooks/pre-commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# A git commit hook to validate the YAML files when committing and
+# will fail the commit if it doesn't parse correctly.
+#
+# To enable this hook, run `ls -s ../../.githooks/pre-commit.sh .git/hooks/pre-commit`.
+
+# Redirect output to stderr.
+exec 1>&2
+
+# run yamllint on all yaml files in the repo
+declare -a changedyamlfiles=($(git diff --diff-filter=ACMR --staged --name-only | grep -E '\.yamllint$|\.yml$|\.yaml$'))
+if [ -n "${changedyamlfiles[*]}" ]; then
+  for filename in "${changedyamlfiles[@]}"; do
+    yamllint --no-warnings --config-file=.yamllint "$filename"
+  done
+fi
